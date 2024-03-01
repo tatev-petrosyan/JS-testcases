f296f502064ed2d8e2a74c22c9e723803f01dc36
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-03-01 16:35:41 +0400

    testcase3.js

---
+++ testcases/testcase3.js
@@ -0,0 +1,38 @@
+const SeleniumCommon3 = require('./common');
+
+async function runTests() {
+    const common = new SeleniumCommon3();
+    try {
+
+ // Go to site
+ await common.openUrl('http://automationexercise.com');
+
+ // Make sure home is visible
+ await common.makeSureElementIsVisibleByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[1]/a/i');
+ 
+ // Find signup button and click 
+ await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[4]/a');    
+
+ // Make sure  Login to your account is visible
+await common.makeSureElementIsVisibleByXpath('//*[@id="form"]/div/div/div[1]/div/h2');
+
+// Enter incorrect email address 
+await common.sendKeysByXpath('//*[@id="form"]/div/div/div[1]/div/form/input[2]', 'tate-petrosyan-96@mail.ru');
+  
+// Enter incorrect password
+await common.sendKeysByXpath('//*[@id="form"]/div/div/div[1]/div/form/input[3]', 'Tate2022');
+
+// Click 'login' button
+await common.clickElementByXpath('//*[@id="form"]/div/div/div[1]/div/form/button');
+
+// Make sure  'Your email or password is incorrect' is visible
+await common.makeSureElementIsVisibleByXpath('//*[@id="form"]/div/div/div[1]/div/form/button');
+
+} catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        await common.quit();
+    }
+}
+
+runTests();
