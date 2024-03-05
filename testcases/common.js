44b5bedea86849c8069cdb103aa8ee63a9795462
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-03-05 20:33:22 +0400

    modify common js

--- testcases/common.js
+++ testcases/common.js
@@ -1,50 +1,10 @@
 
-const { Builder, By, Select, until } = require('selenium-webdriver');
+const { Builder, By, Select, until, LocalFileDetector } = require('selenium-webdriver');
+const fs = require('fs')
 
 class SeleniumCommon {
     constructor() {
         this.driver = new Builder().forBrowser('chrome').build();
-    }
-
-    async openUrl(url) {
-        await this.driver.get(url);
-    }
-
-    async clickElementById(id) {
-        let element = await this.findElementWithRetry(By.id(id));
-        await element.click();
-    }
-
-    async clickElementByXpath(xpath) {
-        let element = await this.findElementWithRetry(By.xpath(xpath));
-        await element.click();
-    }
-
-    async clickElementByName(name) {
-        let element = await this.findElementWithRetry(By.name(name));
-        await element.click();
-    }
-
-    async clickElementByClassName(className) {
-        let element = await findElementWithRetry(By.className(className));
-        await element.click();
-    }
-
-    async clickElementByPartialLinkText(partialLinkText) {
-        let element = await this.findElementWithRetry(By.partialLinkText(partialLinkText));
-        await element.click();
-    }
-
-    async makeSureElementIsVisibleByPartialLinkText(locator) {
-        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.partialLinkText(locator))));
-    }
-
-    async makeSureElementIsVisibleByClassName(locator) {
-        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.className(locator))));
-    }
-
-    async makeSureElementIsVisibleByXpath(locator) {
-        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.xpath(locator))));
     }
 
     async findElementWithRetry(locator, maxRetries = 5, retryDelay = 1500) {
@@ -74,6 +34,54 @@ class SeleniumCommon {
         await this.driver.quit();
     }
 
+    async openUrl(url) {
+        await this.driver.get(url);
+    }
+
+    async clickElementById(id) {
+        let element = await this.findElementWithRetry(By.id(id));
+        await element.click();
+    }
+
+    async clickElementByXpath(xpath) {
+        let element = await this.findElementWithRetry(By.xpath(xpath));
+        await element.click();
+    }
+
+    async clickElementByName(name) {
+        let element = await this.findElementWithRetry(By.name(name));
+        await element.click();
+    }
+
+    async clickElementCheckboxByXpath(xpath) {
+        let element = await this.findElementWithRetry(By.xpath(xpath));
+        await this.driver.executeScript("arguments[0].click();", element);
+
+    }
+
+    async clickElementByClassName(className) {
+        let element = await findElementWithRetry(By.className(className));
+        await element.click();
+    }
+
+    async clickElementByPartialLinkText(partialLinkText) {
+        let element = await this.findElementWithRetry(By.partialLinkText(partialLinkText));
+        await element.click();
+    }
+
+    async makeSureElementIsVisibleByPartialLinkText(locator) {
+        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.partialLinkText(locator))));
+    }
+
+    async makeSureElementIsVisibleByClassName(locator) {
+        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.className(locator))));
+    }
+
+    async makeSureElementIsVisibleByXpath(locator) {
+        await this.driver.wait(until.elementIsVisible(await this.findElementWithRetry(By.xpath(locator))));
+    }
+
+
     async sendKeysByName(locator, text) {
         let element = await this.findElementWithRetry(By.name(locator));
         await element.sendKeys(text);
@@ -84,11 +92,32 @@ class SeleniumCommon {
         await element.sendKeys(text);
     }
 
+    async sendKeysById(locator, text) {
+        let element = await this.findElementWithRetry(By.id(locator));
+        await element.sendKeys(text);
+    }
+
     async selectDropDownValueById(locator, value) {
         let element = await this.findElementWithRetry(By.id(locator));
         let select = new Select(element)
         await select.selectByValue(value);
     }
+
+    async selectLocatorByXpath(locator, xpath) {
+        let element = await this.findElementWithRetry(By.xpath(locator));
+        let select = new Select(element)
+        await select.selectDropDownValueByXpath(xpath);
+    }
+
+    async navigateTo(url) {
+        await this.driver.navigate(url);
+    }
+
+    async sendKeysByXpath(locator, text) {
+        let element = await this.findElementWithRetry(By.xpath(locator));
+        await element.sendKeys(text);
+    }
 }
+
 
 module.exports = SeleniumCommon;
