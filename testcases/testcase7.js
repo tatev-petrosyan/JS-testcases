f4365e2e2a4377b8f8f58d5914a685ef43033600
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-03-07 16:49:11 +0400

    testcase7.js

---
+++ testcases/testcase7.js
@@ -0,0 +1,26 @@
+const SeleniumCommon7 = require('./common');
+
+async function runTests() {
+    const common = new SeleniumCommon7();
+    try {
+        await common.openUrl('http://automationexercise.com');
+
+        // Make sure home is visible
+        await common.makeSureElementIsVisibleByPartialLinkText("Home");
+
+        //Click on 'Test Cases' button
+        await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[5]/a');
+
+        //Verify user is navigated to test cases page successfully
+        await common.navigateTo('https://automationexercise.com/test_cases');
+
+
+
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        await common.quit();
+    }
+}
+
+runTests();
