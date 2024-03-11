a6ebb10a620ff32860b9c583e48f29e8f58b1dde
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-03-11 22:36:53 +0400

    testcase12.js

---
+++ testcases/testcase12.js
@@ -0,0 +1,48 @@
+const SeleniumCommon9 = require('./common');
+
+async function runTests() {
+    const common = new SeleniumCommon9();
+    try {
+        await common.openUrl('http://automationexercise.com');
+
+        // Make sure home is visible
+        await common.makeSureElementIsVisibleByPartialLinkText("Home");
+
+        //Click on 'Products' button
+        await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[2]/a');
+
+        //Hover over first product and click 'Add to cart'// xndir ka 
+        await common.hoverOverByXpath('/html/body/section[2]/div[1]/div/div[2]/div/div[2]/div/div[1]/div[2]');
+        await common.clickElementByXpath('/html/body/section[2]/div[1]/div/div[2]/div/div[2]/div/div[1]/div[2]/div/a');
+        common.wait();
+        //Click 'Continue Shopping' button
+        await common.clickElementByXpath('//*[@id="cartModal"]/div/div/div[3]/button');
+
+
+        //Hover over second product and click 'Add to cart'
+        await common.hoverOverByXpath('/html/body/section[2]/div/div/div[2]/div/div[3]/div[1]/div[1]');
+        await common.clickElementByXpath('/html/body/section[2]/div/div/div[2]/div/div[3]/div[1]/div[1]/div[1]/a');
+
+        //Click 'View Cart' button
+        await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[3]/a');
+
+        //Verify both products are added to Cart
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-1"]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-2"]');
+
+        //Verify their prices, quantity and total price
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-1"]/td[3]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-1"]/td[4]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-1"]/td[5]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-2"]/td[3]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-2"]/td[4]');
+        await common.makeSureElementIsVisibleByXpath('//*[@id="product-2"]/td[5]');
+
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        await common.quit();
+    }
+}
+
+runTests();
---
+++ testcases/testcase8.js
@@ -0,0 +1,36 @@
+const SeleniumCommon8 = require('./common');
+
+async function runTests() {
+    const common = new SeleniumCommon8();
+    try {
+        await common.openUrl('http://automationexercise.com');
+
+        // Make sure home is visible
+        await common.makeSureElementIsVisibleByPartialLinkText("Home");
+
+        //Click on 'Products' button
+        await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[2]/a');
+
+        //Verify user is navigated to ALL PRODUCTS page successfully
+        await common.navigateTo('https://automationexercise.com/products');
+
+        //The products list is visible
+        await common.makeSureElementIsVisibleByXpath('/html/body/section[2]/div/div/div[2]/div');
+        
+        //Click on 'View Product' of first product
+        await common.clickElementByXpath('/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[2]/ul/li/a');
+        
+        //User is landed to product detail page
+        await common.navigateTo('https://automationexercise.com/product_details/1');
+
+        //Verify that detail detail is visible: product name, category, price, availability, condition, brand
+        await common.makeSureElementIsVisibleByXpath("/html/body/section/div/div/div[2]/div[2]/div[2]/div");
+        
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        await common.quit();
+    }
+}
+
+runTests();
