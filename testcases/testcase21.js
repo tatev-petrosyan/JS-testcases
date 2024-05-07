c5e98f49f60f03b8dc09115bd7363748802d71e1
Tatev Petrosyan <tatew.petrosyan@gmail.com>
2024-05-07 15:05:06 +0400

    testcase21.js

---
+++ testcases/testcase21.js
@@ -0,0 +1,36 @@
+const SeleniumCommon21 = require('./common');
+
+async function runTests() {
+    const common = new SeleniumCommon21();
+    try {
+
+        // Go to site
+        await common.openUrl('http://automationexercise.com');
+
+        //Click on 'Products' button
+        await common.clickElementByXpath('//*[@id="header"]/div/div/div/div[2]/div/ul/li[2]/a');
+
+        // Verify user is navigated to ALL PRODUCTS page successfully
+        await common.navigateTo('https://automationexercise.com/products');
+        await common.wait();
+        //Click on 'View Product' button
+        await common.clickElementByXpath('/html/body/section[2]/div/div/div[2]/div/div[2]/div/div[2]/ul/li');
+
+        //Verify 'Write Your Review' is visible
+        await common.makeSureElementIsVisibleByXpath('/html/body/section/div/div/div[2]/div[3]/div[1]/ul/li/a');
+
+        //Enter name, email and review
+        await common.sendKeysByXpath('//*[@id="name"]', "Tatevik");
+        await common.sendKeysByXpath('//*[@id="email"]', 'Tatevik-petrosyan-96@mail.ru');
+        await common.sendKeysByXpath('//*[@id="review"]', 'Hi,I LIKE THE SHOP, THANK YOU FOR YOUR JOB');
+
+        //Click 'Submit' button
+        await common.clickElementByXpath('//*[@id="button-review"]');
+    } catch (error) {
+        console.error('An error occurred:', error);
+    } finally {
+        await common.quit();
+    }
+
+}
+runTests();
